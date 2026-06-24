529cde56c1dbf9998d316defaf9922730ec754ac
saxenasanidhya48-pixel <saxenasanidhya48@gmail.com>
2026-06-24 14:04:45 +0530

    Implement theme setting and update log message
    Added theme management functionality and updated log message.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("WatchVerse Loaded");
+console.log("🎬 WatchVerse Loaded");
+
+function setTheme(theme){
+
+document.body.setAttribute("data-theme", theme);
+
+localStorage.setItem("watchverse-theme", theme);
+
+}
+
+window.addEventListener("load", () => {
+
+const savedTheme =
+localStorage.getItem("watchverse-theme");
+
+if(savedTheme){
+
+document.body.setAttribute(
+"data-theme",
+savedTheme
+);
+
+}
+
+});
